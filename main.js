fe97f97b65751a0d3959da8e1ea4198f84341b38
hosoyamarcos <marcos.hosoya@gmail.com>
2016-10-19 22:33:42 -0200

    Cria inputs com jquery e sem jquery. Sem jquery nao funciona, mas pode funcionar se pesquisar melhor como fazer. Por default so funciona com jquery

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Hosoya on 10/19/16.
+ */
+$(document).ready(function(){
+    $('.useJquery').iCheck({
+        checkboxClass: 'icheckbox_minimal',
+        radioClass: 'iradio_minimal',
+        increaseArea: '20%' // optional
+    });
+});
+
